879d8b49a2f3dd459640f8dfb3202cc1cfca9cf5
Ken Lenhart <omnipaid@gmail.com>
2022-09-12 10:20:32 -0700

    Correct model test placement

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,12 +1,3 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
-
-  it 'has rides alpha method' do
-    six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
-    hurler = six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
-    scrambler = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
-    ferris = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
-
-    expect(six_flags.rides_alpha).to eq([ferris, hurler, scrambler])
-  end
 end
