1ef7c10474041886e5ca7fbceff8064ba3a9a9ab
Ken Lenhart <omnipaid@gmail.com>
2022-09-12 08:49:33 -0700

    Add mechanics/show_spec.rb

---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'mechanics show page' do
+  # Story 2 - Mechanic Show Page
+
+  # As a user,
+  # When I visit a mechanic show page
+  # I see their name, years of experience, and the names of rides they’re working on
+  # And I only see rides that are open
+  # And the rides are listed by thrill rating in descending order (most thrills first)
+
+  describe 'Story 2 - When I visit a mechanic show page' do
+    
+  end
+end
